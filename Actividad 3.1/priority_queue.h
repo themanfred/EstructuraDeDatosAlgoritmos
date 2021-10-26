cf263b036d4c2a18b646750002f3b5b97651c9d9
Thomas Fruend <42932766+themanfred@users.noreply.github.com>
2021-10-26 13:41:13 -0500

    Actividad 3.1

---
+++ Actividad 3.1/priority_queue.h
@@ -0,0 +1,40 @@
+class priority_queue{
+	public:
+		priority_queue();
+		void push(int dato); 
+		void pop(); 			
+		int top(); 
+		bool empty();
+		int size();
+		void print();
+	private:	
+		vector<int> datos;
+};
+
+priority_queue::priority_queue(){
+
+}
+
+void priority_queue::push(int dato){
+
+}
+
+void priority_queue::pop(){
+	
+}
+
+int priority_queue::top(){
+
+}
+
+bool priority_queue::empty(){
+
+}
+
+int priority_queue::size(){
+
+}
+
+void priority_queue::print(){
+
+}
